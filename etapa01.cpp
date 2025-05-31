1cb186414edcff71f61771ff34eb1394390501a6
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-05-31 02:43:45 +0200

    usado as funcoes transpose() e determinant()

--- etapa01.cpp
+++ etapa01.cpp
@@ -5,9 +5,16 @@ using namespace Eigen;
 using namespace std;
 
 int main(){
+    // Matrix 2 x 2
     Matrix2d A;
-    A << 1,2,3,4;
-    cout << "Matriz A:\n" <<A<<endl;
+    A << 4,1,3,2;
+    cout << "Matriz A:\n" <<A<<endl<<endl;
+
+    // Transposta
+    cout << "Transporta A:\n" << A.transpose() << endl<<endl;
+
+    // Determinante
+    cout << "Determinante A:\n" << A.determinant() << endl<<endl;
 
     return 0;
 }
