b2bda629be76c49dc977e7d4305a92cc7b9ebf76
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-02 23:33:56 +0200

    fechado o arquivo apos criacao

--- etapa09.cpp
+++ etapa09.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <fstream>
 
 using namespace std;
 using namespace Eigen;
 
 int main(){
-    
+    Matrix3d A, B,resultado;
+    A << 1,2,3,
+        4,5,6,
+        7,8,9;
 
+    B << 9,8,7,
+         6,5,4,
+         3,2,1;
+    resultado = A + B;
+    cout << "Resultado A + B:\n" << A + B << endl;
+
+    ofstream arquivo("resultado.txt"); 
+
+    if(arquivo.is_open()){
+        arquivo << "Resultado A + B:\n" << A + B << endl;
+        cout << "Arquivo criado com nome resultado\n";
+        arquivo.close();
+    }else{
+        cout << "Erro arquivo nao criado\n";
+    }
     return 0;
 }
