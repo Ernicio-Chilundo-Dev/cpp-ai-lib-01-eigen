941f2a4318a2257ab5d3848d0c0d550cc8f45bf1
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-02 12:07:37 +0200

    Funcoes que fazem as operacoes criadas

--- etapa08_1.cpp
+++ etapa08_1.cpp
@@ -15,3 +15,32 @@ void mostrarResultado(const string& titulo, const Matrix3d& resultado){
     separador();
 }
 
+void somarMatrizes(const Matrix3d& A, const Matrix3d& B){
+    mostrarResultado("Resultado A + B", A + B);
+}
+
+void subtrairMatrizes(const Matrix3d& A, const Matrix3d& B){
+    mostrarResultado("Resultado A - B", A - B);
+}
+
+void produtoElemento(const Matrix3d& A, const Matrix3d& B){
+    mostrarResultado("Produto elemento a elemento  (A ⊙ B)", A.cwiseProduct(B));
+}
+
+void multiplicacaoMatricial(const Matrix3d& A, const Matrix3d& B){
+    mostrarResultado("Multiplicacao matricial (A * B)", A * B);
+}
+
+void multiplicacaoEscalar(const Matrix3d& A){
+    double escalar;
+    cout << "Escalar de K: ";
+    while (!(cin>>escalar))
+    {
+        cout << "Entrada invalida! Digite um numero: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    mostrarResultado("Resultado K * A", escalar * A);
+    
+}
