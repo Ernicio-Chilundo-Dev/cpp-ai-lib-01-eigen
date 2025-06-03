2398f5fa3d62008b8b591abea3422753a2023035
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-03 12:23:43 +0200

    ler uma matriz salva num arquivo .txt e carrega-la novamente para usar no programa com Eigen

--- etapa10.cpp
+++ etapa10.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
-#include <Eigen/Dense>
 #include <fstream>
+#include <sstream>
+#include <Eigen/Dense>
 
 using namespace std;
 using namespace Eigen;
 
 int main(){
+    ifstream arquivo("resultado.txt");
+    string linha;
+    Matrix3d matrizLida;
+    int i = 0;
 
-    
+    if (arquivo.is_open()){
+        getline(arquivo, linha);
+        while (getline(arquivo, linha) && i < 3)
+        {
+            stringstream ss(linha);
+            double val1, val2, val3;
+            ss >> val1 >> val2 >> val3;
+
+            matrizLida(i, 0) = val1;
+            matrizLida(i, 1) = val2;
+            matrizLida(i, 2) = val3;
+
+            i++;
+        }
+
+        arquivo.close();
+        cout << "Matriz linda com sucesso:\n"<< matrizLida<< endl;
+        
+    }else{
+        cout << "Erro ao abrir o arquivo!"<< endl;
+    }
+    return 0;
 }
