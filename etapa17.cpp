9ec9a0c51dc0672eac5406794a5a59cc0f0e2dd4
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-08 09:31:26 +0200

    decomposicao svd

--- etapa17.cpp
+++ etapa17.cpp
@@ -12,7 +12,29 @@ int main(){
     cout << "Digite o numero de colunas da matriz: ";
     cin >> colunas;
 
+    MatrixXd A(linhas,colunas);
+    cout << "Digite os elementos da matriz linha por linha:\n";
+
+    for (int i = 0; i < linhas; ++i)
+    {
+        for(int j = 0; j < colunas; ++j){
+            cout << "A("<<i<<","<<j<<"): ";
+            cin >> A(i,j);
+        }
+    }
     
+    cout << "Matriz A:\n"<< A<<endl<<endl;
+
+    //Decomposicao SVD
+    JacobiSVD<MatrixXd> svd(A, ComputeThinU | ComputeThinV);
+
+    auto U = svd.matrixU();
+    auto S = svd.singularValues().asDiagonal();
+    auto Vt = svd.matrixV();
+
+    
+
+
 
     return 0;
 }
