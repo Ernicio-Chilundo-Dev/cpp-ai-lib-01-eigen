7f0605c2111ddd944c9aadb2ab9b6d868833be0d
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-08 07:03:51 +0200

    decomposicao de matrizes com Eigen(QR,Cholesky,SVD)

---
+++ etapa15.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+int main(){
+    Matrix3d A;
+      Matrix3d A;
+    A << 12, -51, 4,
+         6, 167, -68,
+        -4, 24, -41;
+
+    return 0;
+}
