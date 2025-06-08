5804c5cb305c7252f7a1bd8c40ace70fd1f8843e
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-08 07:39:01 +0200

    cholesky

--- etapa15.cpp
+++ etapa15.cpp
@@ -6,7 +6,6 @@ using namespace Eigen;
 
 int main(){
     Matrix3d A;
-      Matrix3d A;
     A << 12, -51, 4,
          6, 167, -68,
         -4, 24, -41;
@@ -18,5 +17,19 @@ int main(){
         cout << "Matriz A:\n"<<A<<endl<<endl;
         cout << "Matriz Q (ortogonal):\n"<<Q<<endl<<endl;
         cout << "Matriz R (triangular superior):\n"<<R<<endl<<endl;
-    return 0;
+
+        cout << "===============================Cholesky=============================\n";
+         Matrix3d B;
+        B << 4, 1, 1,
+            1, 3, 0,
+            1, 0, 2;
+
+        LLT<Matrix3d> lltOfA(B);
+        Matrix3d L = lltOfA.matrixL();
+
+        cout << "Matriz A:\n"<<A<<"\n\n";
+        cout << "Matriz L (Trriangular inferior):\n"<<L<<"\n\n";
+        cout << "Reconstrucao A = L * Lᵗ:\n"<<L * L.transpose()<<endl<<endl;
+
+ return 0;
 }
