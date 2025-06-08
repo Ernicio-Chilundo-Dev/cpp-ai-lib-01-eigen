6e87953178bc829f1b5a8d74c2ba10fe721b7a4f
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-08 06:54:43 +0200

    encontrando vetores que nao mundam de direcao quando multiplicados por A

--- etapa14.cpp
+++ etapa14.cpp
@@ -4,9 +4,9 @@ using namespace std;
 using namespace Eigen;
 
 int main(){
-    Matrix3d A = (Matrix3d()<< 4,-2,-1,0,2,1,0,0,1).finished();
+    Matrix3d A = (Matrix3d()<< 4,-2,1,0,3,-1,0,0,2).finished();
 
-    cout << "Matriz A:\n";
+    cout << "Matriz A:\n" << A <<endl<<endl;
 
     //Calculando Autovalores e Autovetores
     EigenSolver<Matrix3d> solver(A);
