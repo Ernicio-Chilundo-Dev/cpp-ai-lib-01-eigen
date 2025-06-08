3a8b7a5049f76ee7a15e525121db6f6dc06086be
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-08 08:28:22 +0200

    etapa 16 compressao de dados em SVD com Eigen

--- etapa16.cpp
+++ etapa16.cpp
@@ -5,6 +5,15 @@ using namespace std;
 using namespace Eigen;
 
 int main(){
+    MatrixXd A(5,5);
+
+        A << 1,2,3,4,5,
+             5,4,3,2,1,
+             2,3,4,3,2,
+             1,1,1,1,1,
+             0,1,0,1,0;
+
+    cout << "Matriz Original (simulado uma imagem):\n" << A << "\n\n";
 
     return 0;
 }
