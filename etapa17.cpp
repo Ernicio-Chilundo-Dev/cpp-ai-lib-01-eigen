b6554f7f6788f2159f3d4ba660f3c0d998ed1bae
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-08 09:17:07 +0200

    programa interativo

---
+++ etapa17.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+int main(){
+    int linhas, colunas;
+
+    cout << "Digite o numero de linhas da matriz: ";
+    cin >> linhas;
+    cout << "Digite o numero de colunas da matriz: ";
+    cin >> colunas;
+
+    
+
+    return 0;
+}
