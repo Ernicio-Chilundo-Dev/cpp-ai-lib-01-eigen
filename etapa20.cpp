193fecc086ac8bd74aa281af15c07524f560499f
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-06-09 19:59:41 +0200

    calculado a media

--- etapa20.cpp
+++ etapa20.cpp
@@ -10,7 +10,24 @@ double distancia(const RowVectorXd& a, RowVectorXd& b){
 }
 
 int mai(){
+      // Classe A (3 amostras 2D)
+    MatrixXd A(3, 2);
+    A << 2.5, 2.4,
+         2.2, 2.9,
+         2.3, 2.7;
 
+    // Classe B (3 amostras 2D)
+    MatrixXd B(3, 2);
+    B << 0.5, 0.7,
+         1.0, 1.1,
+         0.8, 0.6;
 
+    // Unir dados
+    MatrixXd dados(6,2);
+    dados << A, B;
+
+    //centralizar
+    RowVectorXd media = dados.colwise().mean();
+    MatrixXd centralizado = dados.rowwise() - media;
     return 0;
 }
